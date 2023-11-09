024f9f6e60e1aecda0c819fc6e001c901e87632e
Logan Furlong <furlong_logan@columbusstate.edu>
2023-11-09 04:44:40 +0000

    added useeffects in the body hooks

--- src/components/BodyHooks.jsx
+++ src/components/BodyHooks.jsx
@@ -1,12 +1,27 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 function BodyHooks(){
     const [count, setCount] = useState(0)
+    const [count2, setCount2] = useState(10)
+
+    useEffect(()=> {
+        console.log(count)
+    }, [count])
+
+    useEffect(()=> {
+        console.log(count2)
+    }, [count2])
 
     return(
-        <button type="button" className="btn btn-dark" onClick={setCount(count + 1)}>
-            {this.state.count}
+        <>
+        <button type="button" className="btn btn-dark" onClick={() => {setCount(count + 1)}}>
+            {count}
         </button>
-    )
+
+        <button type="button" className="btn btn-primary" onClick={() => {setCount(count2 + 1)}}>
+            {count2}
+        </button>
+        </>
+    )   
 }
 export default BodyHooks;
